05556659ad953011db878324d673fb3f1445df4e
kleebeng <2214748998@qq.com>
2023-06-11 11:19:59 +0800

    添加项目文件。

---
+++ sudoku/sudoku.cpp
@@ -0,0 +1,50 @@
+﻿// sudoku.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include"head.h"
+
+int main(int argc, char** argv) {
+	if (argc == 1) {	//未输入命令行参数，报错并返回
+		cout << "please input command in cmd line" << endl;
+		return 0;
+	}
+	else {
+		if (!strcmp(argv[1], "-c")) {
+			int produce_num = atoi(argv[2]);
+			//若无法转换则代表存在非法输入
+			if (produce_num != 0) {
+				int produce_num = atoi(argv[2]);
+				if (produceOutputIntoTxt(produce_num) == -1) {
+					cout << "终局生成失败" << endl;
+					return -1;
+				}
+			}
+			else {
+				cout << "please input number after -c" << endl;
+				return 0;
+			}
+		}
+		else if (!strcmp(argv[1], "-s")) {
+			if (dealQuestion(argv[2]) == -1) {
+				cout << "error deal sudoku question" << endl;
+				return -1;
+			}
+		}
+		else {	//错误输入，报错
+			cout << "input cmd error!" << endl;
+			return 0;
+		}
+	}
+}
+
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
